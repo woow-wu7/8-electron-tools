b9c626bb48237c176efb91e1b7cf4a6e904d7f43
woow-wu7 <woow.wu7@gmail.com>
2023-05-29 22:10:25 +0800

    feat: Browserwindow.

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,29 @@
-import { defineConfig } from 'vite'
-import electron from 'vite-plugin-electron'
-import renderer from 'vite-plugin-electron-renderer'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import electron from "vite-plugin-electron";
+import renderer from "vite-plugin-electron-renderer";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 7777,
+  },
   plugins: [
     vue(),
     electron([
       {
         // Main-Process entry file of the Electron App.
-        entry: 'electron/main.ts',
+        entry: "electron/main.ts",
       },
       {
-        entry: 'electron/preload.ts',
+        entry: "electron/preload.ts",
         onstart(options) {
-          // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete, 
+          // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete,
           // instead of restarting the entire Electron App.
-          options.reload()
+          options.reload();
         },
       },
     ]),
     renderer(),
   ],
-})
+});
