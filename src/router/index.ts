2422bd82a47dc511a0f9c1aaa0d2dd4b13619ff2
woow-wu7 <woow.wu7@gmail.com>
2023-06-03 17:02:57 +0800

    feat: record/delete record.

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,14 +4,9 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/1111",
+      path: "/",
       name: "record",
       component: () => import("../views/Recorder.vue"),
-    },
-    {
-      path: "/",
-      name: "record2",
-      component: () => import("../views/Recorder2.vue"),
     },
     {
       path: "/wallpaper",
--- src/views/Recorder2.vue
+++ src/views/Recorder2.vue
@@ -1,355 +1,119 @@
 <template>
-  <section>
-    <div class="record__top">
-      <div class="record__left">
-        <div class="start__container">
-          <div class="start__button" @click="onStartRecord">
-            <el-icon class="start__icon" v-if="!state.isStaring"
-              ><VideoPlay
-            /></el-icon>
-            <el-icon class="start__icon loading-clockwise" v-else
-              ><Refresh
-            /></el-icon>
-            <div class="start__text" v-if="!state.isStaring">开始录屏</div>
-            <div class="start__text" v-else>{{ time ? time : "00:00:00" }}</div>
-          </div>
-        </div>
-
-        <div class="full__container">
-          <div class="full__button" @click="onEndRecord">
-            <el-icon class="full__icon"><VideoPause /></el-icon>
-            <div class="full__text">结束录屏</div>
-          </div>
-        </div>
-      </div>
-
-      <div class="record__content">
-        <img
-          :src="state.previewImageUrl"
-          class="screen__img"
-          v-if="!state.recordUrl"
-        />
-        <video
-          :src="`http://localhost:7778/${state.recordUrl}`"
-          v-else
-          class="screen__img"
-          width="300"
-          height="400"
-          autoplay
-        ></video>
+  <section ref="recordRef">
+    <div class="start__layer" v-show="state.isShow">
+      <div class="button__start" @click="onStartRecord">
+        <el-icon class="loading-clockwise font-30" v-if="state.loading"
+          ><Refresh
+        /></el-icon>
+        <div v-else>录屏</div>
       </div>
     </div>
-
-    <div class="record__list">
-      <!-- <div class="record__title">视频播放列表</div> -->
-      <div
-        v-for="(item, i) in state.recordList"
-        :class="['record__item', { 'is-selected': item.isSelected }]"
-        @click="onSelect(item, i)"
-      >
-        <div class="record__name">{{ item.name }}</div>
-        <div class="record__play" @click="onPlay(item)">播放</div>
-        <div class="record__del" @click="onDelete(item)">删除</div>
-        <!-- <div class="record__time">{{ item.time }}</div>
-        <div class="record__size">{{ item.size }}</div> -->
-      </div>
+    <video src="" ref="videoRef" autoplay muted></video>
+    <div class="end__layer" @click="onStopRecord" v-show="!state.isShow">
+      <div class="button__end">关闭</div>
     </div>
   </section>
 </template>
 
 <script setup lang="ts">
-import { reactive, onMounted, onUnmounted } from "vue";
-const { ipcRenderer } = require("electron");
-import { useTimer } from "../utils/hooks/useTimer";
-import { httpServer } from "../server/server";
+import { ref, reactive } from "vue";
 
-const fs = require("fs");
-const path = require("path");
+const videoRef = ref();
+const recordRef = ref();
 
-const { time, clearTimer, run: timerRun } = useTimer();
-console.log("time", time);
-
-const state = reactive<any>({
-  previewImageUrl: "",
-  isStaring: false,
-
-  recordRef: {},
-  recordList: [],
-  recordUrl: "",
+const state = reactive({
+  isShow: true,
+  loading: false,
 });
-
-onMounted(() => {
-  initPreviewImg();
-  initRecordList();
-  httpServer();
-});
-onUnmounted(() => {
-  clearTimer();
-});
-
-const getPreviewResource = () => {
-  return new Promise((resolve) => {
-    ipcRenderer.send("start-get-desktop-source");
-    ipcRenderer.on("end-get-desktop-source", (e, data) => {
-      console.log("e", e);
-      resolve(data);
-    });
-  });
-};
-
-const initPreviewImg = async () => {
-  const source: any = await getPreviewResource();
-  state.previewImageUrl = source.thumbnail.toDataURL();
-};
-
-const initRecordList = () => {
-  state.recordList = readVideo();
-  console.log("state.recordList", state.recordList);
-};
-
-const getStream = async () => {
-  const source: any = await getPreviewResource();
-  const stream = await navigator.mediaDevices.getUserMedia({
-    audio: false,
-    video: {
-      // @ts-ignore
-      mandatory: {
-        chromeMediaSource: "desktop",
-        chromeMediaSourceId: source.id,
-        minWidth: 1280,
-        maxWidth: 1280,
-        minHeight: 720,
-        maxHeight: 720,
-      },
-    },
-  });
-  console.log("stream", stream);
-  return stream;
-};
 
 const onStartRecord = async () => {
-  state.isStaring = true;
-  timerRun();
-
-  const stream = await getStream();
-  startRecord(stream);
+  await init();
+  state.isShow = false;
 };
 
-const onEndRecord = () => {
-  clearTimer();
-  state.isStaring = false;
-  state.recordRef.onstop();
-};
-
-const startRecord = async (stream: any) => {
-  const list: any = [];
-  state.recordRef = new MediaRecorder(stream, {
-    mimeType: "video/webm",
+const init = async () => {
+  state.loading = true;
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: {
+      width: recordRef.value.offsetWidth,
+      height: recordRef.value.offsetHeight - 10,
+    },
   });
-  if (state.recordRef) {
-    state.recordRef.start(1000);
-    state.recordRef.ondataavailable = (event: any) => {
-      list.push(event.data);
-    };
-    state.recordRef.onstop = async () => {
-      console.log("stop");
-      const blob = new Blob([...list], {
-        type: "video/webm",
-      });
-      saveVideo(blob).then(() => {
-        console.log("保存成功 ");
-        state.recordList = readVideo();
-      });
-    };
-    state.recordRef.onerror = (err: any) => {
-      console.log("err", err);
-    };
-  }
+  videoRef.value.srcObject = stream;
+  videoRef.value.play();
+  state.loading = false;
 };
 
-const readVideo = () => {
-  if (fs.existsSync("/Users/xiawu/Downloads/video")) {
-    const videoNames = fs.readdirSync("/Users/xiawu/Downloads/video");
+const onStopRecord = () => {
+  state.isShow = true;
 
-    console.log("videoNames", videoNames);
-    const fileNames = videoNames.filter((item: any) => {
-      const filePath = path.join(`/Users/xiawu/Downloads/video/${item}`);
-      console.log("33333", fs.statSync(filePath));
-      return fs.statSync(filePath) && item.match(/.mp4$/);
-    });
-
-    return fileNames.map((item: any) => ({
-      name: item,
-    }));
-  }
+  videoRef.value.srcObject.getTracks()[0].stop();
 };
 
-const saveVideo = (blob: any) => {
-  return new Promise((resolve, reject) => {
-    const times = new Date().getTime();
-
-    if (!fs.existsSync("/Users/xiawu/Downloads/video")) {
-      fs.mkdirSync("/Users/xiawu/Downloads/video", { recursive: true });
-    }
-
-    const reader: any = new FileReader();
-    reader.readAsArrayBuffer(blob);
-    reader.onload = () => {
-      console.log("111", reader);
-      const buffer = Buffer.from(reader?.result);
-      fs.writeFile(
-        `/Users/xiawu/Downloads/video/${times}.mp4`,
-        buffer,
-        {},
-        (err: any) => console.log(err)
-      );
-    };
-
-    reader.onloadend = () => {
-      resolve(true);
-    };
-    reader.onerror = (err: any) => {
-      reject(err);
-    };
-  });
-};
-
-const onSelect = (item: any, i: number) => {
-  state.recordList.forEach((current: any, index: any) => {
-    if (index === i) {
-      current.isSelected = true;
-    } else {
-      current.isSelected = false;
-    }
-  });
-};
-
-const onPlay = (item: any) => {
-  state.recordUrl = item.name;
-};
-
-const onDelete = (item: any) => {};
-
-// const init = async () => {
-//   const stream = await navigator.mediaDevices.getUserMedia({
-//     video: {
-//       width: recordRef.value.offsetWidth,
-//       height: recordRef.value.offsetHeight - 10,
-//     },
+// const onShareScreen = async () => {
+//   const stream = await navigator.mediaDevices.getDisplayMedia({
+//     video: { width: 600, height: 300, displaySurface: "window" },
+//     audio: false,
 //   });
+//   console.log("stream", stream);
 //   videoRef.value.srcObject = stream;
 //   videoRef.value.play();
 // };
+
+// 注意
+// 这里渲染进程 之所以可以使用node是因为 我们在 electron main.js 中 设置了 ( nodeIntegration contextIsolation )
+// const { ipcRenderer } = require("electron");
+
+// 发数据
+// const send = () => {
+//   ipcRenderer.send("message_from_ipcRenderer", "渲染进程to主进程");
+// };
+
+// 接收数据
+// ipcRenderer.on("message_from_ipcMain", (e, data) => {
+//   console.log("e", e);
+//   console.log("data", data);
+// });
 </script>
 
 <style scoped lang="scss">
 section {
   flex: 1;
   position: relative;
-  background: #262839;
-  color: #fff;
-  box-sizing: border-box;
 
-  @include flex-column();
+  .start__layer {
+    @include flex-center();
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background: #262839;
+    z-index: 999;
 
-  .record__top {
-    height: 200px;
-    box-sizing: border-box;
-    border-bottom: 1px solid #191919;
-    @include flex-r-between-center();
-
-    .record__left {
-      @include flex-r-start-center();
-      flex: 1;
-
-      .full__container {
-        flex: 1;
-      }
-      .start__container,
-      .full__container {
-        height: 200px;
-        width: 200px;
-        @include flex-center();
-        position: relative;
-
-        .start__button,
-        .full__button {
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          cursor: pointer;
-
-          .start__icon,
-          .full__icon {
-            font-size: 40px;
-            color: #6c78c7;
-          }
-          .start__text,
-          .full__text {
-            margin-top: 10px;
-            font-size: 14px;
-            color: #7077a3;
-          }
-        }
-      }
-
-      .button__full {
-        flex: 1;
-        height: 200px;
-        @include flex-center();
-      }
-    }
-
-    .record__content {
-    }
-  }
-
-  .record__list {
-    box-sizing: border-box;
-    padding-top: 10px;
-
-    .record__title {
-      padding: 12px 30px;
-      color: #6c78c7;
-      border-bottom: 1px solid #191919;
-    }
-
-    .record__item {
-      padding: 12px 30px;
-      color: #6c78c7;
-      font-size: 14px;
+    .button__start {
+      @include flex-center();
+      width: 130px;
+      height: 130px;
+      background: #1a1f23;
+      border-radius: 100%;
+      border: 6px solid #25b493;
+      color: #fff;
+      box-shadow: -1px -1px -1px 100px #1a1f23;
       cursor: pointer;
-
-      @include flex-r-start-center();
-
-      .record__name {
-        width: 220px;
-      }
-
-      .record__play {
-        &:hover {
-          color: #76c2af;
-        }
-      }
-
-      .record__del {
-        margin-left: 20px;
-        &:hover {
-          color: rgb(255, 72, 105);
-        }
-      }
-
-      .record__time {
-        margin-right: 30px;
-      }
     }
   }
 
-  .screen__img {
+  .end__layer {
+    @include flex-center();
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 30px;
+    background: #1f272f;
+    cursor: pointer;
+    color: #fff;
   }
-}
-.is-selected {
-  background: #315359;
 }
 </style>
